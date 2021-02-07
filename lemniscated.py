738a5c6701da05042d4c8cea02131705b9261b00
unknown <timfrederickbinauhan@gmail.com>
2021-02-07 16:23:37 +0800

    First commit

---
+++ lemniscated.py
@@ -0,0 +1,32 @@
+messages = []
+chat = [' ']
+message = ' '
+user = ''
+
+class User:
+    warnings = 0
+    warn_reason = ''
+
+    def __init__(self):
+        pass
+    def warn(self, reason):
+        warnings += 1
+        warn_reason + reason
+    def pardon(self):
+        warnings -= 1
+        warn_reason = ''
+
+class Jon:
+    def __init__(self):
+        self.isMod = True
+        self.isAdmin = True
+        self.isPerson = True
+        self.isBot = False
+        self.isMad = False
+    def purge(self):
+        if self.isMad:
+            messages.append(message)
+            chat.pop(messages.index(message))
+            User.warn('You can\'t send that thing')
+        else:
+            pass
