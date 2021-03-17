5182858a983f62df791652c2f0f0cc1dfb9c6d4a
KotonBads <66117568+KotonBads@users.noreply.github.com>
2021-03-17 08:12:22 +0800

    Update lemniscated.py

--- lemniscated.py
+++ lemniscated.py
@@ -1,21 +1,33 @@
-#this is where the variables live :)
-messages = []
-chat = [' ']
-message = ' '
-user = ''
+class Chat:
+    def __init__(self, message):
+        self.message = message
+        self.chat_file_w = open('lemniscated\chat.txt', 'w')
+    
+    def save_chat(self):
+        self.chat_file_w.write(self.message)
 
 class User:
     warnings = 0
-    warn_reason = ''
+    warn_reason = []
+    mute_time = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, message):
+        self.isMuted = False
+        self.canChat = True
+        self.message = message
+
     def warn(self, reason):
         warnings += 1
-        warn_reason + reason
+        warn_reason.append(reason)
+
     def pardon(self):
         warnings -= 1
         warn_reason = ''
+    
+    def mute(self, time):
+        mute_time = time
+        self.isMuted = True
+        self.canChat = False
 
 class Jon:
     def __init__(self):
@@ -24,15 +36,20 @@ class Jon:
         self.isPerson = True
         self.isBot = False
         self.isMad = False
-    def purge(self):
+
+    def purge(self, amount):
         if self.isMad:
-            messages.append(message)
-            chat.pop(messages.index(message))
+            
             User.warn('You can\'t send that thing')
         else:
             pass
-    def warn(self):
-        User.warn('Just don\'t...')
+
+    def warn(self, reason):
+        User.warn(reason)
+
     def pardon(self):
         User.pardon()
     
+    def mute_member(self, time):
+        if self.isMad:
+            User.mute(time)
